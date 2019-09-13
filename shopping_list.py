cc6728d1ab9cab8640033ae499ed153dfa1f9ecd
Akash Siddharth <14816427+AkashSiddharth@users.noreply.github.com>
2019-09-13 13:12:07 -0400

    TestCode

---
+++ shopping_list.py
@@ -0,0 +1,15 @@
+def shopping_list(*small_talk: tuple, **grocery_list: dict) -> int:
+	''' The function takes all the values of the shopping list
+passed as a dictionary and returns the total bill.
+'''
+	if input('Is the shop open? ') in ('Yes', 'yes', 'Y', 'y'):
+		bill: int = 0
+		for words in small_talk:
+			print(words)
+		for item, quantity in grocery_list.items():
+			print ("Give me ", item, ": ", quantity, " Kg")
+			bill += 5 * int(quantity)
+		return bill
+
+# Call the function
+shopping_list('Great!','I was hoping for it', 'Here is my Grocery list: ', Maida=5, Sugar=2, Atta='10')
