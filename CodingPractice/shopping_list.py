274b365ff51344e8d807350c739e1eadf2f33f26
Akash Siddharth <14816427+AkashSiddharth@users.noreply.github.com>
2019-10-11 11:36:36 -0400

    Advance A.I. Assignment-Initial Commit

--- CodingPractice/shopping_list.py
+++ CodingPractice/shopping_list.py
@@ -2,7 +2,7 @@ def shopping_list(*small_talk: tuple, **grocery_list: dict) -> int:
 	''' The function takes all the values of the shopping list
 passed as a dictionary and returns the total bill.
 '''
-	if input('Is the shop open? ') in ('Yes', 'yes', 'Y', 'y'):
+	if input('Is the shop open? ') in {'Yes', 'yes', 'Y', 'y'}:
 		bill: int = 0
 		for words in small_talk:
 			print(words)
@@ -11,5 +11,13 @@ passed as a dictionary and returns the total bill.
 			bill += 5 * int(quantity)
 		return bill
 
+# print function doc
+print(shopping_list.__doc__)
+
+# print function annotation
+print(shopping_list.__annotations__)
+
 # Call the function
-shopping_list('Great!','I was hoping for it', 'Here is my Grocery list: ', Maida=5, Sugar=2, Atta='10')
+bill = shopping_list('Great!','I was hoping for it', 'Here is my Grocery list: ', Maida=5, Sugar=2, Atta='10')
+
+print ("Shopping Bill : ", bill)
